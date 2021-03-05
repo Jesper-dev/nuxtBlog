ea6d2a73f0b61af7cfea7e32066f43e1e92d84dc
JesperKYH <jesper.pettersson@student.kyh.se>
2021-03-05 08:37:16 +0100

    Initialzied project and made some starting files

---
+++ README.md
@@ -0,0 +1,4 @@
+# blog
+
+This is a project from a udemy course, [Udemy](https://www.udemy.com/course/nuxtjs-vuejs-on-steroids/)
+"# nuxtBlog" 
---
+++ pages/posts/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="post-page">
+    <h1>This is the post page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .post-page {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
